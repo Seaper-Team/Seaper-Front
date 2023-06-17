817259dbe23c1d79e7518541e0a557151baa7648
Xiaoyi311 <1908351852@qq.com>
2023-06-17 21:14:24 +0800

    Better Package

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,13 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebPackPlugin = require('clean-webpack-plugin').CleanWebpackPlugin;
+const UglifyParallelPlugin = require('webpack-parallel-uglify-plugin');
 
 module.exports = {
   entry: {
-    main: "./src/main.ts",
+    seaper: "./src/main.ts",
   },
   output: {
-    filename: "[name].js",
+    filename: "js/[name].bundle.js",
     path: path.join(__dirname, "../dist"),
   },
   module: {
@@ -35,12 +40,64 @@ module.exports = {
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        rc: {
+          name: "rc",
+          test: (module) => (/rc-/.test(module.context)),
+          priority: 4
+        },
+        react: {
+          name: "react",
+          test: (module) => (/react/.test(module.context) || /redux/.test(module.context)
+            || /classnames/.test(module.context) || /prop-types/.test(module.context)),
+          priority: 3
+        },
+        antd: {
+          name: "antd",
+          test: (module) => (/antd/.test(module.context) || /ant-design/.test(module.context)),
+          priority: 2
+        },
+        default: {
+          name: "other",
+          test: (module) => (/node_modules/.test(module.context)),
+          priority: 1
+        }
+      }
+    },
+    runtimeChunk: {
+      name: entrypoint => `rt-${entrypoint.name}`
+    },
+    minimizer:[
+      new CssMinimizerPlugin(),
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: "./index.html",
+      filename: './index.html',
+      chunks: "all"
     }),
-    new MiniCssExtractPlugin({
-      filename: "main.css"
+    new MiniCssExtractPlugin(),
+    new BundleAnalyzerPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, '../public'),
+          to: path.resolve(__dirname, '../dist'),
+        }
+      ]
+    }),
+    new CleanWebPackPlugin(),
+    new UglifyParallelPlugin({
+      uglifyJS: {
+        output: {
+          beautify: false,
+          comments: false
+        }
+      }
     })
   ],
   mode: "production",
